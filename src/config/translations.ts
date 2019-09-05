1ba18244907e85f1455879eda05d4c410edf9f45
anthones <antoniedimovski@gmail.com>
2019-09-05 05:31:39 +0200

    ux improvement

--- src/config/translations.ts
+++ src/config/translations.ts
@@ -2,7 +2,7 @@ export const translations = {
   en: {
     genericLoginError: "Invalid login credentials.",
     specificLoginError:
-      "Sorry about that, our back-end devs were probably drunk when they made the API",
+      "Sorry about that, our back-end devs were probably drunk when they made the API. Please try again.",
     timeLoginError: "Too many failed attempts. Please wait 30 seconds.",
     loginContent: "Log in to your account",
     username: "Username",
